b74cb4458851d9569a995164642f4ec067118ad9
aannovru <aannovru@gmail.com>
2011-01-16 23:57:56 +0300

    my new aannovru heroku app

---
+++ _site/helpers/new_post.rb
@@ -0,0 +1,68 @@
+#!/usr/bin/env ruby
+
+ROOT_DIR_RELATIVE_PATH = '../'
+
+def chdir_to_root_dir
+  Dir.chdir File.join(Dir.pwd, ROOT_DIR_RELATIVE_PATH)
+end
+
+# script's entry point
+# community  links  news  solution  stories
+category = case ARGV[0]
+when /community/i
+  'community'
+when /links/i
+  'links'
+when /news/i
+  'news'
+when /solution/i
+  'solution'
+else
+  # show last_id and exit
+  nil
+end
+
+puts "category = #{category}"
+
+# change dir to the site's root dir
+chdir_to_root_dir
+puts 'root_dir = ' + Dir.pwd
+
+#files = Dir.glob('**/*-id-*')
+ids = Dir.glob('**/*-id-*').map do |f|
+  ($1).to_i if File.basename(f, '.*') =~ /id-(\d+)/
+end
+
+ids.sort!
+last_id = ids[-1] || 0
+last_id = last_id + 1
+
+puts "last_id = #{last_id}"
+exit unless category
+
+filename = Time.now.strftime("%Y-%m-%d-id-") +  "%05d" % last_id + ".md"
+puts "filename = #{filename}"
+
+fullpath = File.join(Dir.pwd, '_posts', category, filename)
+puts "fullpath = #{fullpath}"
+
+# ---
+#title: Молитва Нашей Шаговой Группы
+#layout: post
+#category: solution
+#tags: [ Молитвы ]
+#author:
+#---
+File.open(fullpath, 'w') do |f|
+  f.puts "---"
+  f.puts "title: "
+  f.puts "layout: post"
+  f.puts "category: #{category}"
+  f.puts "tags: [  ]"
+  f.puts "author:"
+  f.puts "---"
+  f.puts
+  f.puts
+end
+
+# vim: set sts=2 sw=2 et:
---
+++ _site/helpers/patch_defines.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+
+require 'tempfile'
+
+ROOT_DIR_RELATIVE_PATH = '../'
+DEFINES_FILE = '_includes/defines.html'
+FILES = %w(
+  _layouts/post.html
+  community/index.html
+  links/index.html
+  news/index.html
+  solution/index.html
+  stories/index.html
+  index.html
+)
+
+def chdir_to_root_dir
+  Dir.chdir File.join(Dir.pwd, ROOT_DIR_RELATIVE_PATH)
+end
+
+def patch_file defines_contents, filename
+  f = Tempfile.new('ruby_helpers')
+  puts "Processing: " + filename
+  puts "temp file = #{f.path}"
+  defines_contents.each { |line| f.puts line }
+  found = false
+  File.readlines(filename).each { |line|
+    next unless line =~ /^private:/ or found
+    # MP: UGLY
+    found = true
+    f.puts line.chop!
+  }
+  File.rename(f.path, filename)
+  f.delete  
+end
+
+# script's entry point
+# change dir to the site's root dir
+chdir_to_root_dir
+puts 'root_dir = ' + Dir.pwd
+puts 'defines_file = ' + DEFINES_FILE
+puts 'files to patch:'
+FILES.each { |f| puts f }
+
+defines_contents = [ '---' ]
+
+File.readlines(DEFINES_FILE).each { |line|
+  defines_contents << line.chop! unless line =~ /^---/
+}
+
+#defines_contents.each { |s| puts s }
+FILES.each { |file| patch_file defines_contents, file }
+
+
+# vim: set sts=2 sw=2 et:
